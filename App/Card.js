1c3e2ea8172925fc16379c8222e765740086e9a0
Joao Tonial <joao.tonial@beehiveclub.com.br>
2018-08-16 19:18:24 -0300

    flipping coin

--- App/Card.js
+++ App/Card.js
@@ -1,36 +1,88 @@
 import React, { Component } from 'react';
-import { Text, View, Animated, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, Animated, TouchableOpacity, StyleSheet, Platform } from 'react-native';
 
 export default class Card extends Component {
   constructor(props){
     super(props);
-    this.state = { flipValue: new Animated.Value(0) };
+    this.state = { flipValue: new Animated.Value(0), isFlipped: false};
+    this.interpolateFront = this.state.flipValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['0deg', '180deg'],
+    });
+    this.interpolateBack = this.state.flipValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['180deg', '360deg'],
+    });
   }
 
-  handleFlip(){
-    Animated.timing(this.state.flipValue,{
-      toValue: 180,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
+  UNSAFE_componentWillMount() {
+    const { flipValue } = this.state;
+    if (Platform.OS === 'android') {
+      Animated.sequence([
+        Animated.timing(flipValue, {
+          toValue: 180,
+          duration: 1,
+          useNativeDriver: true,
+        }),
+        Animated.timing(flipValue, {
+          toValue: 0,
+          duration: 1,
+          useNativeDriver: true,
+        }),
+      ]).start();
+    }
   }
+
+  handleFlip() {
+    const { flipValue, isFlipped } = this.state;
+
+    let flipConfig = {
+      friction: 6,
+      tension: 20,
+      useNativeDriver: true
+    };
+    if(isFlipped){
+      flipConfig = {
+        ...flipConfig,
+        toValue: 0,
+      };
+    } else {
+      flipConfig = {
+        ...flipConfig,
+        toValue: 180,
+      }
+    }
+    Animated.spring(flipValue, flipConfig).start();
+    this.setState({isFlipped: !isFlipped});
+  }
+
 
   render() {
+    const frontAnimatedStyle = {
+      transform: [
+        { rotateY: this.interpolateFront },
+      ]
+    };
+    const backAnimatedStyle = {
+      transform: [
+        { rotateY: this.interpolateBack },
+      ]
+    };
     return (
       <View style={styles.container} >
         <View>
-          <View style={styles.cardFront}>
+          <Animated.View style={[styles.cardFront, frontAnimatedStyle]}>
             <Text style={styles.cardText} >
               This Is a Card
             </Text>
-          </View>
-          <View style={[styles.cardFront, styles.cardBack]} >
+          </Animated.View>
+          <Animated.View style={[backAnimatedStyle, styles.cardFront, styles.cardBack ]} >
             <Text style={styles.cardText}>
               This Is a Card Back
             </Text>
-          </View>
+          </Animated.View>
         </View>
-        <TouchableOpacity style={styles.button} onPress={() => this.handleFlip}>
+        <TouchableOpacity style={styles.button} onPress={() => this.handleFlip()}>
           <Text style={[styles.cardText, {color: '#aaaaaa'}]} >flip</Text>
         </TouchableOpacity>
       </View>
@@ -45,12 +97,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   cardFront: {
-    width: 230,
-    height: 350,
+    width: 270,
+    height: 330,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#560D00',
     backfaceVisibility: 'hidden',
+    zIndex: 10
   },
   cardBack: {
     backgroundColor: '#040100',
@@ -69,5 +122,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     top: 10,
-  }
+  },
 });
