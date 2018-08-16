1b76c7dc2761c32a4cdf9e68ef19d8f99c419af1
Joao Tonial <joao.tonial@beehiveclub.com.br>
2018-08-16 11:41:43 -0300

    style ready

--- App/Card.js
+++ App/Card.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Animated, TouchableWithoutFeedback } from 'react-native';
+import { Text, View, Animated, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class Card extends Component {
   constructor(props){
@@ -17,17 +17,22 @@ export default class Card extends Component {
 
   render() {
     return (
-      <View styles={styles.container} >
-        <TouchableWithoutFeedback onPress={() => this.handleFlip}>
-          <View styles={styles.cardFront}>
-            <Text styles={styles.cardText} >
-            This Is a Card
+      <View style={styles.container} >
+        <View>
+          <View style={styles.cardFront}>
+            <Text style={styles.cardText} >
+              This Is a Card
             </Text>
           </View>
-          <View styles={styles.cardBack}>
-            <Text> This Is a Card Back </Text>
+          <View style={[styles.cardFront, styles.cardBack]} >
+            <Text style={styles.cardText}>
+              This Is a Card Back
+            </Text>
           </View>
-        </TouchableWithoutFeedback>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={() => this.handleFlip}>
+          <Text style={[styles.cardText, {color: '#aaaaaa'}]} >flip</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -36,17 +41,33 @@ export default class Card extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   cardFront: {
-    backbroundColor: '#040100',
-  },
-  cardBack: {
-    width: '70%',
-    height: '80%',
-    backgroundColor: '#ECAFA4',
+    width: 230,
+    height: 350,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#560D00',
     backfaceVisibility: 'hidden',
   },
+  cardBack: {
+    backgroundColor: '#040100',
+    position: 'absolute',
+    top: 0,
+  },
   cardText: {
-    color: '#fff'
+    color: '#fff',
+    fontFamily: 'monospace',
+    fontSize: 15,
+  },
+  button: {
+    backgroundColor: '#fff',
+    width: 60,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 10,
   }
 });
